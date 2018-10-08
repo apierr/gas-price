7dd5a71bf34aef4abc9cbde346883c49af1093b9
apierro <antonio.pierro@gmail.com>
2018-10-08 15:32:23 +0200

    added threading timer

--- extract.py
+++ extract.py
@@ -1,32 +1,67 @@
-import wget, time
+import subprocess, time
+from user_agent import generate_user_agent as user_agent
+from threading import Timer
 
 class Extract:
 
     def __init__(self):
         self.start_time = time.time()
-        self.max_requests_per_token = 2 #200
+        self.max_requests_per_token = 190 #200
         self.tokens = [
             '8f8751bd9aa7419fb9ca04c72b47002f',
             'ca1af79ec91947c0845a4c1e0794a568',
             '74991f4042ae47f689deb587b8d3ed54',
-            'a3ff4595a44e4c21a4ae621cfc58d376'
+            'a3ff4595a44e4c21a4ae621cfc58d376',
+            '533f8c572729432f91579df65ca6d8fb'
         ]
-        self.url = 'https://api.blockcypher.com/v1/eth/main/txs'
+        self.minutes = int(self.max_requests_per_token * 2 * len(self.tokens) / 30) + 1
+        self.urls = {
+            'blockcypher_txs': 'https://api.blockcypher.com/v1/eth/main/txs',
+            'ethergasstation': 'https://ethgasstation.info/json/ethgasAPI.json',
+            'etherchain': 'https://www.etherchain.org/api/gasPriceOracle',
+            'network_stats': 'https://api.ethpool.org/networkStats',
+            'pool_stats': 'https://api.ethpool.org/poolStats',
+            'block_cypher_pool_stats': 'https://api.blockcypher.com/v1/eth/main'
+        }
 
-    def _get_file_name(self, i):
-        return str(i) + '_' + self.token[0:2] +'_txs.json'
+    def _get_file_name(self, url):
+        return 'output/' + str(int(time.time())) + '_' + url + '.json'
 
-    def _get_url(self):
-        return self.url + '?token=' + self.token
+    def _get_subprocess_args(self, url):
+        return [
+            'wget',
+            self._get_url(url),
+            '-O',
+            self._get_file_name(url)
+        ]
 
-    def _download_tx(self, i):
-        wget.download(url = self._get_url(), out = self._get_file_name(i))
-        time.sleep(2.0 - ((time.time() - self.start_time) % 2.0))
+    def _get_url(self, url):
+        if url == 'blockcypher_txs':
+            return self.urls[url] + '?token=' + self.token
+        return self.urls[url]
 
-    def download_txs(self):
-        for self.token in self.tokens:
-            for i in range(self.max_requests_per_token): self._download_tx(i)
+    def _download_every_second(self, token):
+        self.token = token
+        subprocess.call(self._get_subprocess_args('blockcypher_txs'))
+
+    def _download_every_minute(self, url):
+        subprocess.call(self._get_subprocess_args(url))
+
+    def download_every_second(self):
+        interval = 0
+        for token in self.tokens:
+            for i in range(1, self.max_requests_per_token + 1):
+                Timer(interval, self._download_every_second, [token]).start()
+                interval = interval + 2
+
+    def download_every_minute(self):
+        for i in range(self.minutes):
+            for url in self.urls:
+                if url == 'blockcypher_txs':
+                    continue
+                Timer(i * 60.0, self._download_every_minute, [url]).start()
 
 if __name__ == '__main__':
     extract = Extract()
-    txs.download_txs()
+    extract.download_every_second()
+    extract.download_every_minute()
--- metadata_db.py
+++ metadata_db.py
@@ -5,26 +5,54 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 class Transaction (Base):
+    # https://api.blockcypher.com/v1/eth/main/txs (ok)
     __tablename__ = 'transaction'
     tx_id = Column(Integer, primary_key = True)
-    tx_hash = Column(String(64), unique = True)
     tx_ts = Column(Integer, nullable = False)
+    tx_hash = Column(String(64), unique = True)
     tx_gas_limit = Column(Integer, nullable = True)
-    gas_price = Column(Numeric, nullable = True)
-    block_id = Column(Integer, ForeignKey('block.block_id'), nullable = True)
+    tx_gas_price = Column(Numeric, nullable = True)
+    bck_id = Column(Integer, ForeignKey('block.block_id'), nullable = True)
 
 class Block (Base):
+    # https://api.blockcypher.com/v1/eth/main/blocks/7
     __tablename__ = 'block'
-    block_id = Column(Integer, primary_key = True)
-    block_hash = Column(String(64), unique = True)
-    block_ts = Column(Integer, nullable = False)
-    block_gas_limit = Column(Integer, nullable = True)
-    block_time = Column(Integer, nullable = False)
+    bck_id = Column(Integer, primary_key = True)
+    bck_ts = Column(Integer, nullable = False)
+    bck_hash = Column(String(64), nullable = False, unique = True)
+    bck_gas_limit = Column(Integer, nullable = True)
+    bck_time = Column(Integer, nullable = True)
     reward = Column(Numeric, nullable = True)
     fees = Column(Numeric, nullable = True)
 
+class NetworkStats (Base):
+    # https://api.ethpool.org/networkStats
+    __tablename__ = 'networkstats'
+    ns_id = Column(Integer, primary_key = True)
+    ns_ts = Column(Integer, nullable = False)
+    difficulty = Column(Integer, nullable = False)
+    ns_hashrate = Column(Numeric, nullable = False)
+    usd = Column(Numeric, nullable = True)
+    btc = Column(Numeric, nullable = True)
+
+class PoolStats (Base):
+    # https://api.ethpool.org/poolStats
+    __tablename__ = 'poolstats'
+    ps_id = Column(Integer, primary_key = True)
+    ps_ts = Column(Integer, nullable = False)
+    ps_hashrate = Column(Numeric, nullable = False)
+    miners =  Column(Integer, nullable = False)
+    workers = Column(Integer, nullable = False)
+    blocksPerHour = Column(Numeric, nullable = False)
+
+class BlockCypherPoolStats
+    # https://api.blockcypher.com/v1/eth/main
+    __tablename__ = 'blockcypherpoolstats'
+    bcps_id = Column(Integer, primary_key = True)
+    bcps_ts = Column(Integer, nullable = False)
+
 class EtherGasStation (Base):
-    # https://ethgasstation.info/json/ethgasAPI.json
+    # https://ethgasstation.info/json/ethgasAPI.json (ok)
     __tablename__ = 'ethergasstation'
     egs_id = Column(Integer, primary_key = True)
     egs_ts = Column(Integer, nullable = False)
@@ -41,7 +69,7 @@ class EtherGasStation (Base):
     egs_safeLow = Column(Numeric, nullable = True)
 
 class EtherChain (Base):
-    # https://www.etherchain.org/api/gasPriceOracle
+    # https://www.etherchain.org/api/gasPriceOracle (ok)
     # https://www.etherchain.org/tools/gasPriceOracle
     __tablename__ = 'etherchain'
     ec_id = Column(Integer, primary_key = True)
